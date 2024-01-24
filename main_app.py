0ef744d425348f23de894fb3784f635bc6ccc170
SaudRamai <136465879+SaudRamai@users.noreply.github.com>
2024-01-24 17:12:08 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,10 +1,46 @@
+
 import streamlit as st
 from PIL import Image
 import json
 import requests  
 from langchain.callbacks import get_openai_callback  
-from backend_api.database import DatabaseConnection
 from backend_api.visualization import Visualization
+from backend_api.database import DatabaseConnection
+from backend_api.visualization import generate_visualization
+import hmac
+import streamlit as st
+
+
+def check_password():
+
+    def login_form():
+        st.set_page_config(page_title="LexoGraph", layout="wide")
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        if st.session_state["username"] in st.secrets["passwords"] and hmac.compare_digest(st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if st.session_state.get("password_correct", False):
+        return True
+
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()
 
 def main():
     if "history" not in st.session_state:
